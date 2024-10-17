a89ffb33ef91ac87e4dd5e6d7ab20173ee21c856
Jiayi Qiu <jiayiqw1004@gmail.com>
2024-10-17 18:44:52 +0100

    Create IfStatements.py

---
+++ IfStatements.py
@@ -0,0 +1,11 @@
+num1 = int(input("Input your first number"))
+num2 = int(input("Input your second number"))
+
+if num1 < num2:
+    print(num2, "is larger")
+elif num1 > num2:
+    print(num1, "is bigger!!!")
+else:
+    print("they're equal unlike our society")
+
+print("Sum of num1 and num2:", num1+num2)
