896d496aa20f805fa986b4c06b175aff906ce9db
Jiayi Qiu <jiayiqw1004@gmail.com>
2024-10-24 22:41:01 +0100

    Create PasswordTrueStatement.py

---
+++ PasswordTrueStatement.py
@@ -0,0 +1,14 @@
+while(True):
+    print("hi can you give me your password?")
+    password=input()
+    if password=="password":
+        print("correct")
+    else:
+        print("try again")
+    print("Yay! Logged in!")
+
+#ctrl c interrupts code
+#count = 1
+#while (count < 4):
+    #print("yay I can count")
+    #count = count +1
